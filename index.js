d3fdcf740db920d92287b2cdbebc844a80a2a97e
Daa <75169859+Adhaaz@users.noreply.github.com>
2024-05-14 14:21:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,16 @@ bot.start((ctx) => ctx.reply('Welcome!'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on('sticker', (ctx) => ctx.reply('????'));
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.command('server', (ctx) => {
+  const serverInfo = `
+Server Information:
+- Platform: ${process.platform}
+- Architecture: ${process.arch}
+- Node.js Version: ${process.version}
+- CPU Cores: ${require('os').cpus().length}
+`;
+
+  ctx.reply(serverInfo);
+});
 
 bot.launch();
