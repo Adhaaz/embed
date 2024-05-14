b2b56f2486505c2a1ba9efb6af8962b3548915fb
Daa <75169859+Adhaaz@users.noreply.github.com>
2024-05-14 16:22:22 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const os = require('os');
 const { execSync } = require('child_process');
 const fs = require('fs');
 const path = require('path');
+const ytdl = require('ytdl-core');
 
 
 const bot = new Telegraf('6817115625:AAGZ2bt9sDF11Y41gRrQTCNIJuqjweTVDE0');
@@ -12,6 +13,34 @@ bot.start((ctx) => ctx.reply('Welcome!'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on('sticker', (ctx) => ctx.reply('????'));
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+
+
+bot.command('download', async (ctx) => {
+  try {
+    // Mendapatkan URL video dari pengguna
+    const url = ctx.message.text.split(' ')[1];
+
+    // Menggunakan ytdl-core untuk mendapatkan informasi video
+    const info = await ytdl.getInfo(url);
+
+    // Membuat pesan dengan informasi video
+    const message = `
+Title: ${info.videoDetails.title}
+Duration: ${info.videoDetails.lengthSeconds} seconds
+Size: ${(info.videoDetails.filesize / 1024 / 1024).toFixed(2)} MB
+`;
+
+    // Mengirim pesan ke pengguna
+    await ctx.reply(message);
+
+    // Mengunggah video ke chat
+    await ctx.replyWithVideo({ url: info.videoDetails.video_url });
+  } catch (error) {
+    // Menangani kesalahan
+    console.error(error);
+    await ctx.reply('Terjadi kesalahan saat mengunduh video.');
+  }
+});
 
 bot.command('server', (ctx) => {
   // Get deploy information
